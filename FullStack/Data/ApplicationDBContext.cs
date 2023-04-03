6b839f6eb70b9b0e8208a14d44434a67d7e030d5
Chiemela <Chiemela242@gmail.com>
2023-04-03 18:32:55 +0100

    Init updates

---
+++ FullStack/Data/ApplicationDBContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using FullStack.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
+namespace FullStack.Data
+{
+    public class ApplicationDBContext : IdentityDbContext
+    {
+        public ApplicationDBContext(DbContextOptions options) : base(options)
+        {
+            
+        }
+
+        public DbSet<SalesLeadEntity> SalesLead { get; set; }
+    }
+}
